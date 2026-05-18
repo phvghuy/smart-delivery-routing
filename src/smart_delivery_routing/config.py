85f62e548cd76d2024152e01556173fefb5151b9
Huy Pham <huypham9724@gmail.com>
2026-05-18 08:10:54 +0700

    fix: allow import without env vars to unblock CI test collection

--- src/smart_delivery_routing/config.py
+++ src/smart_delivery_routing/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 OSRM_URL = os.environ.get("OSRM_URL", "http://localhost:5000")
